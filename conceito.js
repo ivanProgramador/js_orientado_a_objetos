51a44fe687037cb576c93b94f6ee39dfa0203ddb
ivan <ivan-guita@hotmail.com>
2024-03-26 00:50:17 -0300

    conceitos fim da aula 157

---
+++ conceito.js
@@ -0,0 +1,13 @@
+/*
+A orientação a objetos é um paradigma de programação,
+atualmente suportado pela maioria das linguagens.
+
+Apoiada em 5 pilares 
+- Herança 
+- Polimorfismo
+- encapsulamento
+- abstração
+- composição
+
+Apatir dessa lista eu vou começar os estudos avançados 
+*/
