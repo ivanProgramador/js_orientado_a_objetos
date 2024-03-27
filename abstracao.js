759cddc3fca67a8d03ad5e8b5753d9b2de3bd5cd
ivan <ivan-guita@hotmail.com>
2024-03-26 22:00:10 -0300

    conceitos de abstração fim da aula 159

---
+++ abstracao.js
@@ -0,0 +1,11 @@
+/*
+A abstratção pode ser vista como simplificação na orintação a objetos,
+a asbtração consiste em pegar apenas as caracteriscas convenienetes a o
+contexto por exemplo:
+a classe filme para o sistema da netflix e direnete de um filme para o 
+cadastro de uma locadora  
+
+
+
+
+*/
