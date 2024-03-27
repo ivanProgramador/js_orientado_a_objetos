900254364d28fa49228af549c3aa6add2379cb1b
ivan <ivan-guita@hotmail.com>
2024-03-27 09:26:30 -0300

    abstração parte 2 fim da  aula 159

--- abstracao.js
+++ abstracao.js
@@ -2,8 +2,34 @@
 A abstratção pode ser vista como simplificação na orintação a objetos,
 a asbtração consiste em pegar apenas as caracteriscas convenienetes a o
 contexto por exemplo:
-a classe filme para o sistema da netflix e direnete de um filme para o 
-cadastro de uma locadora  
+
+a classe filme para o sistema da Netflix e diferente de um filme para o 
+cadastro de uma locadora porque as duas tem regras de negocio diferentes
+por exemplo em uma locadora o filme precisa ter o status de alugado ou diponivel,
+na Netlix isso não importa porque quando o cliente faz uma conta ele pode assistir 
+todos os filmes que ele quiser, isso se chama abstração é a simplificação de alguma coisa 
+com base no contexto.
+
+Lógico que um filme tem muitas caracteristicas mais complexas como:
+
+- diretor 
+- escritor 
+- cameras usadas no set 
+- produtora 
+- estudio
+- elenco
+- duração 
+- custo total de produção  
+
+porém para uma locadora oque interessa:
+
+- data de lançamento
+- o cliente que alugou
+- preço cobrado pelo tempo
+- se o filme esta disponivel ou alugado  
+
+isso é abstração simplificar algo confirme o contexto.
+
 
 
 
